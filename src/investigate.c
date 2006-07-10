df63dab14f5dfa6fda44ffec690238de9ae32a25
Adam Goode <agoode@andrew.cmu.edu>
2006-07-10 20:21:24 +0000

    almost done

--- src/investigate.c
+++ src/investigate.c
@@ -28,14 +28,20 @@ void on_startSearch_clicked (GtkButton *button,
     gtk_tree_view_get_selection(GTK_TREE_VIEW(glade_xml_get_widget(g_xml,
 								   "definedSearches")));
 
+  g_debug("selection: %p", selection);
+
   if (gtk_tree_selection_get_selected(selection,
-				      saved_search_store,
+				      &saved_search_store,
 				      &s_iter)) {
     gdouble r_min;
     gdouble r_max;
 
-    gtk_tree_model_get(saved_search_store, &s_iter, 1, &r_min, 2, &r_max, -1);
+    g_debug("saved_search_store: %p", saved_search_store);
+    gtk_tree_model_get(GTK_TREE_MODEL(saved_search_store),
+		       &s_iter,
+		       1, &r_min, 2, &r_max, -1);
 
+    g_debug("searching from %g to %g", r_min, r_max);
 
     // build model
   }
