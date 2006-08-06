a9537fac0ed6eb3794577390e3b6f22504b1b5f6
Adam Goode <agoode@andrew.cmu.edu>
2006-08-06 17:47:20 +0000

    fixup circles2

--- src/circles2.c
+++ src/circles2.c
@@ -32,7 +32,7 @@ typedef struct {
 
 
 // static parameters, used from GUI
-static circles_state_t staticState =
+static const circles_state_t staticState =
   {1, 0, 2, 400, 5, 10, 2, 100, 1.0, NULL, NULL};
 
 
