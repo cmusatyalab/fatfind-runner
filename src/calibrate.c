c0d02acfca8482956851f28f004d3f2ca19788e0
Adam Goode <agoode@andrew.cmu.edu>
2006-09-12 18:02:27 +0000

    rework copyright

--- src/calibrate.c
+++ src/calibrate.c
@@ -1,17 +1,17 @@
-/******************************************************************************
+/*
  * This file is part of FatFind,
- *   a Diamond-based system for adipocyte exploration
+ * a Diamond-based system for adipocyte exploration.
  *
  * Copyright (c) 2006 Carnegie Mellon University. All rights reserved.
  * Additional copyrights may be listed below.
  *
- * This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution in the file named LICENSE.
+ * This program and the accompanying materials are made available under
+ * the terms of the Eclipse Public License v1.0 which accompanies this
+ * distribution in the file named LICENSE.
  *
- * Contributors:
- *    Adam Goode - initial implementation
- *****************************************************************************/
+ * Technical and financial contributors are listed in the file named
+ * CREDITS.
+ */
 
 #ifdef HAVE_CONFIG_H
 #  include <config.h>
--- src/calibrate.h
+++ src/calibrate.h
@@ -1,13 +1,17 @@
-/******************************************************************************
- * Copyright (c) 2006 Carnegie Mellon University.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+/*
+ * This file is part of FatFind,
+ * a Diamond-based system for adipocyte exploration.
  *
- * Contributors:
- *    Adam Goode - initial implementation
- *****************************************************************************/
+ * Copyright (c) 2006 Carnegie Mellon University. All rights reserved.
+ * Additional copyrights may be listed below.
+ *
+ * This program and the accompanying materials are made available under
+ * the terms of the Eclipse Public License v1.0 which accompanies this
+ * distribution in the file named LICENSE.
+ *
+ * Technical and financial contributors are listed in the file named
+ * CREDITS.
+ */
 
 #ifndef CALIBRATE_H
 #define CALIBRATE_H
