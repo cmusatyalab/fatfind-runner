9765d47fa66b6a27018b57723e31c5380e66b3cb
Adam Goode <agoode@andrew.cmu.edu>
2010-03-16 10:55:08 -0400

    Fix call to ls_ref_attr

--- src/investigate.cpp
+++ src/investigate.cpp
@@ -131,7 +131,7 @@ static void foreach_select_investigation(GtkIconView *icon_view,
 
   GdkPixbufLoader *pix_loader = gdk_pixbuf_loader_new();
   void *diamond_data;
-  unsigned int len;
+  size_t len;
   ls_ref_attr(newobj, "", &len, (unsigned char **) &diamond_data);
 
   // loop to work around crash
